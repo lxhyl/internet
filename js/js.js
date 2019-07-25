58c7e8a964087b9ea558960b7788563e060dfc84
lxhyl <3529725395@qq.com>
2019-07-25 18:50:23 +0800

    zpf

---
+++ js/js.js
@@ -0,0 +1,34 @@
+
+Vue.component('shouye', {
+    template: '#shouye'
+})
+Vue.component('lishi', {
+    template: '#lishi'
+})
+Vue.component('fazhan', {
+    template: '#fazhan'
+})
+Vue.component('zhuangkuang', {
+    template: '#zhuangkuang'
+})
+Vue.component('yingyong', {
+    template: '#yingyong'
+})
+Vue.component('yingxiang', {
+    template: '#yingxiang'
+})
+Vue.component('weilai', {
+    template: '#weilai'
+})
+
+var vm = new Vue({
+    el:'#app',
+    data:{
+        comName: 'shouye',
+       
+
+    },
+    methods:{
+       
+    }
+})
