84453385bad20d4b131d5abea22c6b39718bde24
lxhyl <3529725395@qq.com>
2019-07-31 21:49:51 +0800

    by-zpf

--- js/js.js
+++ js/js.js
@@ -1,6 +1,93 @@
 
 Vue.component('shouye', {
-    template: '#shouye'
+    template: '#shouye',
+    data(){
+        return {
+            spanBgd1:[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ],
+            spanBgd2:[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ],
+            spanBgd3:[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ],
+            spanBgd4:[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ],
+            spanBgd5:[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ],
+            spanBgd6:[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ],
+        }
+    },
+    methods:{
+        change1:function(){
+            this.spanBgd1=[
+                {backgroundColor:"#039BE5",border:"solid #81D4FA 5px"}
+            ]
+        },
+        change2:function(){
+            this.spanBgd2=[
+                {backgroundColor:"#039BE5",border:"solid #81D4FA 5px"}
+            ]
+        },
+        change3:function(){
+            this.spanBgd3=[
+                {backgroundColor:"#039BE5",border:"solid #81D4FA 5px"}
+            ]
+        },
+        change4:function(){
+            this.spanBgd4=[
+                {backgroundColor:"#039BE5",border:"solid #81D4FA 5px"}
+            ]
+        },
+        change5:function(){
+            this.spanBgd5=[
+                {backgroundColor:"#039BE5",border:"solid #81D4FA 5px"}
+            ]
+        },
+        change6:function(){
+            this.spanBgd6=[
+                {backgroundColor:"#039BE5",border:"solid #81D4FA 5px"}
+            ]
+        },
+
+        leave1:function(){
+            this.spanBgd1=[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ]
+        },
+        leave2:function(){
+            this.spanBgd2=[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ]
+        },
+        leave3:function(){
+            this.spanBgd3=[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ]
+        },
+        leave4:function(){
+            this.spanBgd4=[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ]
+        },
+        leave5:function(){
+            this.spanBgd5=[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ]
+        },
+        leave6:function(){
+            this.spanBgd6=[
+                {backgroundColor:"#94874e",border:"solid #c6a300 5px"}
+            ]
+        }
+        
+    }
+    
 })
 Vue.component('lishi', {
     template: '#lishi'
@@ -25,7 +112,7 @@ var vm = new Vue({
     el:'#app',
     data:{
         comName: 'shouye',
-       
+        
 
     },
     methods:{
